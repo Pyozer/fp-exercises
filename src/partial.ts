48290a53ba0b0c99660cc3b47ea27e8137440514
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2020-08-31 12:14:29 +0200

    Add partial

--- src/partial.ts
+++ src/partial.ts
@@ -4,7 +4,13 @@ and returns a new function that is Fn partially applied to the arguments.
 */
 type FuncPartial<T> = (...args: any[]) => T
 
-const partial = <T, U>(func: FuncPartial<T>): T => {
-    // TODO
-    return null;
+const partial = <T>(f: FuncPartial<T>, ...inputArgs: any[]): FuncPartial<T> => {
+    return f.bind(null, ...inputArgs);
 }
+
+const sum = (...numbers: number[]): number => numbers.reduce((p, c) => p + c, 0)
+const concatSpaced = (a: string, b: string): string => `${a} ${b}`;
+
+console.log(partial(sum, 1, 2, 3, 6)(12, 24)); // 1 + 2 + 3 + 6 + 12 + 24 = 48
+console.log(partial(concatSpaced, 'Bye')('bye'));
+console.log(partial(concatSpaced)('Bye', 'bye'));
